97ee7c90667c6ade42a7adec26033eb3d7c33c63
Yaxh <yaxh@example.com>
2025-12-30 11:36:48 +0530

    Fix CORS to allow production frontend regardless of NODE_ENV

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use((req, res, next) => {
 
 // Middleware
 app.use(cors({
-  origin: '*',
-  credentials: false,
+  origin: ['https://civicspot-frontend.vercel.app', 'http://localhost:3000', 'http://localhost:5173'],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
